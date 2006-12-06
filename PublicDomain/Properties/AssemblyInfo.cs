b67ea76b8298ab217f5289b4d9c5b7e82087630f
SND\schizoidboy_cp <SND\schizoidboy_cp@11aa8812-6de0-41ed-92bd-ec94540cc92c>
2006-12-06 22:11:02 +0000

    Added strong name
    git-svn-id: https://publicdomain.svn.codeplex.com/svn@13282 11aa8812-6de0-41ed-92bd-ec94540cc92c

--- PublicDomain/Properties/AssemblyInfo.cs
+++ PublicDomain/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PublicDomain")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Public Domain code")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MyDorp")]
+[assembly: AssemblyCompany("Public Domain")]
 [assembly: AssemblyProduct("PublicDomain")]
-[assembly: AssemblyCopyright("Copyright © MyDorp 2006")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Public Domain")]
+[assembly: AssemblyTrademark("Public Domain")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -31,5 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
